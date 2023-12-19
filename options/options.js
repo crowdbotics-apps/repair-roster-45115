784d97cb2ca2b7c943a9bd9f0f4961d35fcba445
Crowdbotics <team@crowdbotics.com>
2023-12-19 20:45:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "repair-roster-45115",
+  url: "https://repair-roster-45115.botics.co",
+  api: "https://repair-roster-45115.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
